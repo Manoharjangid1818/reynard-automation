32b94e0636e88188ffdef2f04699fac8bdd53b41
techie <techie18@email.com>
2026-07-05 21:57:26 +0530

    Restore deleted files

---
+++ tests/navigation.spec.js
@@ -0,0 +1,13 @@
+const { test } = require('../utils/authFixture');
+const { loadTestData, getEnabledTests, tcTitle } = require('../utils/testHelpers');
+const { runGenericSanityCase } = require('../utils/sanityHelpers');
+
+const data = loadTestData('navigation');
+
+test.describe('Navigation', () => {
+  for (const tc of getEnabledTests(data)) {
+    test(tcTitle(tc), async ({ authenticatedPage }) => {
+      await runGenericSanityCase(authenticatedPage, tc);
+    });
+  }
+});
